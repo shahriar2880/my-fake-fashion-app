ed4e16e67ebd5c4cba41e494edc074db1742080d
shahriar2880 <shahriarreza436@gmail.com>
2025-05-10 02:12:51 +0600

    local storage functionalities

---
+++ src/utilities/localStorage.jsx
@@ -0,0 +1,20 @@
+const getStoredCart = () =>{
+    const storedCartString = localStorage.getItem('cart');
+    if (storedCartString){
+        return JSON.parse(storedCartString)
+    }
+    return [];
+}
+
+const saveToCart = (cart)  => {
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem("cart", cartStringified)
+}
+
+const addToLocalStorage = (id) =>{
+    const cart = getStoredCart();
+    cart.push(id);
+    saveToCart(cart)
+}
+
+export {addToLocalStorage}
